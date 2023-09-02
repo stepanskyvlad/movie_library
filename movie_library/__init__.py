ad565a325eb03df82b2ce1cec20c46337a3991c8
Vladyslav Yarema <vlad14yarema@gmail.com>
2023-09-02 19:12:19 +0300

    added dark and light mode

--- movie_library/__init__.py
+++ movie_library/__init__.py
@@ -10,11 +10,14 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config["MONGODB_URI"] = os.environ.get("MONGODB_URI")
+    # app.config["MONGODB_URI"] = os.environ.get("MONGODB_URI")
+    client = MongoClient(os.getenv("MONGO_URI"))
     app.config["SECRET_KEY"] = os.environ.get(
         "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw"
     )
-    app.db = MongoClient(app.config["MONGODB_URI"]).get_default_database()
+    # app.db = MongoClient(app.config["MONGODB_URI"]).get_default_database()
+    app.db = client.new
 
     app.register_blueprint(pages)
+
     return app
